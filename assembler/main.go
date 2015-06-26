98206175a6d8a344d2822159338f57c251025359
Harry Culpan <harry@tesseract.org>
2015-06-25 21:35:29 -0400

    Reading command line for assembler

--- assembler/main.go
+++ assembler/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
   "fmt"
+  "errors"
+  "flag"
   )
 
 func main() {
   fmt.Println("Hucs Assembler")
+
+  outFilenamePtr := flag.String("of", nil, "Output filename")
+  flag.Parse()
+  fmt.Println("outFilename=" + *outFilenamePtr)
+  fmt.Println(flag.Args())
+}
+
+func usage() {
+  fmt.Println("usage: hucsas [-of=outputfile] <input filename>")
+  fmt.Println("-of    set output filename")
 }
